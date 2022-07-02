14ef119f8e4743d1b3295ddecdaf04fce486c236
Janneck <janneck.denda@gmail.com>
2022-07-02 11:27:50 +0200

    /wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,64 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <IoAbstractionWire.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Wire.h>
+#include <Adafruit_MCP23X17.h>
+#include <Servo.h>
 
-// Define Input Connections
-#define CH1 3
-#define CH2 5
-#define CH3 6
-#define CH4 9
-#define pwmPin 2
+#pragma region VARS & Definitions
+/*
+  initialize all MCPS
+*/
+Adafruit_MCP23X17 mcp1; 
+Adafruit_MCP23X17 mcp2;  // Used for REMOTE AND DISPLAY
+
+Servo lenker; 
+int lenkerPos = 0;    
+
+#define DebugPin 5
+#define DebugLed 4
+/*
+  Define all pins used for Remote and Motor Control
+ */
+#define CH1 9 //Channels for remote
+#define CH2 6
+#define CH3 5
+#define CH4 3
+#define pwmPin 10 //PWM for hacked motor driver
+
+#define ReceiveLED 9
+
+/*
+    Define Racing Panel Input
+*/
+#define EngineStart 7
+#define SwitchMaster  3
+#define Switch1 6
+#define Switch2 5
+#define Switch3 4
+
+#define BlinkerLinks 7
+#define BlinkerRechts 6
  
-// Integers to represent values from sticks and pots
+/*
+  Storages 
+*/
 int ch1Value;
 int ch3Value;
-int ch4Value; 
+int ch4Value, ch4LastValue; 
 int multi; 
 int counter = 0; 
 int time = 0; 
- 
-// Boolean to represent switch value
 bool ch2Value;
+bool debug = false; 
 
-const int rs = 7, en = 8, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
+/*
+  defining all needed for display
+*/
+const int rs = 2, en = 4, d4 = 7, d5 = 11, d6 = 12, d7 = 13;
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-//progress bar character for brightness
 int pBari = 0;         // progress bar
 byte pBar[8] = {
   B11111,
@@ -33,17 +70,22 @@ byte pBar[8] = {
   B11111,
 };
 
-//S curve
+/*
+  Defining mapping for S Curve to normalize Speed Control
+*/
 int s[] = {0,23, 46, 69, 92, 115, 138, 161, 184, 207, 230, 255};
 int in[] = {0, 48, 69, 88, 97, 118, 135, 154, 174, 202, 222, 255}; 
+
+#pragma endregion
  
+#pragma region functions
 // Read the number of a specified channel and convert to the range provided.
 // If the channel is off, return the default value
 int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
   int ch = pulseIn(channelInput, HIGH, 30000);
   if (ch < 100) return defaultValue;
   int mapped = map(ch, 875, 2140, minLimit, maxLimit);
-  if ((mapped < 5) && (mapped > -5)){
+  if ((mapped < 10) && (mapped > -10)){
     return 0;
   }else{
     return mapped;
@@ -58,7 +100,23 @@ bool readSwitch(byte channelInput, bool defaultValue){
 }
 
 void writeSpeedToMotor(int speed){
-    analogWrite(pwmPin, speed);
+  analogWrite(pwmPin, speed);
+}
+
+void controlLenker(int val){
+  if (val == 0){
+    lenker.write(90);
+
+  }else{
+    int mapped = map(val, 10, 255, 90,180);
+    Serial.println(mapped);
+    lenker.write(mapped);
+  }
+
+  // if (val == 0){
+  //   lenker.write(135);
+  // }
+  // lenker.write(val);
 }
 
 void displaySpeed(int speed, int multi){
@@ -82,7 +140,6 @@ void displaySpeed(int speed, int multi){
     lcd.setCursor(i, 1);   
     lcd.write(byte(0));  
   };
-
 }
 
 // note: the _in array should have increasing values
@@ -104,10 +161,14 @@ int multiMap(int val, int* _in, int* _out, uint8_t size)
   return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
 }
  
+#pragma endregion 
+
+#pragma region Setup
 void setup(){
   // Set up serial monitor
   Serial.begin(115200);
-  
+  lenker.attach(8);
+
   // Set all pins as inputs for Remote
   pinMode(CH1, INPUT);
   pinMode(CH2, INPUT);
@@ -123,9 +184,42 @@ void setup(){
   lcd.print("Second line");
   //Create the progress bar character
   lcd.createChar(0, pBar);
+
+  mcp1.begin_I2C(0x20);      // Start MCP 1
+  mcp2.begin_I2C(0x24);      // Start MCP 2
+  
+  mcp1.pinMode(ReceiveLED, OUTPUT); // Define GPB0 on MCP1 as Output
+  mcp1.digitalWrite(ReceiveLED, LOW);
+
+  mcp1.pinMode(EngineStart, INPUT);
+  mcp1.pinMode(SwitchMaster, INPUT);
+  mcp1.pinMode(Switch1, INPUT);
+  mcp1.pinMode(Switch2, INPUT);
+  mcp1.pinMode(Switch3, INPUT);
+
+  mcp2.pinMode(BlinkerLinks, OUTPUT);
+  mcp2.digitalWrite(BlinkerLinks, LOW);
+  mcp2.pinMode(BlinkerRechts, OUTPUT);
+  mcp2.digitalWrite(BlinkerRechts, LOW);
+
+  mcp2.pinMode(DebugPin, INPUT);
+  mcp2.pinMode(DebugLed, OUTPUT);
+  mcp2.digitalWrite(DebugLed, LOW);
 }
+
+#pragma endregion 
  
+#pragma region Loop
+
 void loop() {
+
+  /*
+    Control DEBUG STATE & Visual representation through a LED
+  */
+  if (mcp2.digitalRead(DebugPin) == HIGH){
+    debug = !debug; 
+  }
+  mcp2.digitalWrite(DebugLed, debug);
   
   // Get values for each channel
   ch1Value = readChannel(CH1, -255, 255, 0);
@@ -133,31 +227,21 @@ void loop() {
   ch3Value = readChannel(CH3, -255, 255, 0);
   ch4Value = readChannel(CH4, -255, 255, 0);
   multi = multiMap(ch3Value, in, s, 12);
+  int sum = ch1Value + ch2Value + ch3Value + ch4Value; 
   
   // Print to Serial Monitor
-  Serial.print("Ch1: ");
-  Serial.print(ch1Value);
-  Serial.print(" | Ch2: ");
-  Serial.print(ch2Value);
-  Serial.print(" | Ch3: ");
-  Serial.print(ch3Value);
-  Serial.print(" | Mapped: ");
-  Serial.print(multi);
-  Serial.print(" | Ch4: ");
-  Serial.println(ch4Value);
-
-  
-  // if (ch3Value > 5 ){
-  //     if (counter <= 10){
-  //       Serial.print(time);
-  //       Serial.print(" | ");
-  //       Serial.println(ch3Value);
-  //     }
-  //     counter ++;
-  // }
-
-
-  
+  if (debug){
+    Serial.print("Ch1: ");
+    Serial.print(ch1Value);
+    Serial.print(" | Ch2: ");
+    Serial.print(ch2Value);
+    Serial.print(" | Ch3: ");
+    Serial.print(ch3Value);
+    Serial.print(" | Mapped: ");
+    Serial.print(multi);
+    Serial.print(" | Ch4: ");
+    Serial.println(ch4Value); 
+  }
 
 
   // Write calculated value to motor driver 
@@ -165,6 +249,55 @@ void loop() {
     writeSpeedToMotor(ch3Value);
     displaySpeed(ch3Value, multi);
   }
-  time = time + 500; 
-  delay(500);
+
+  if(ch1Value > 10){
+    mcp2.digitalWrite(BlinkerLinks, HIGH);
+  }else{
+    mcp2.digitalWrite(BlinkerLinks, LOW);
+  }
+  if(ch1Value < -10){
+    mcp2.digitalWrite(BlinkerRechts, HIGH);
+  }else{
+    mcp2.digitalWrite(BlinkerRechts, LOW);
+  }
+
+
+  if(((ch4LastValue +10) <= ch4Value)or (ch4Value > 25)){
+    controlLenker(ch4Value);
+  }else if (ch4Value == 0){
+    controlLenker(ch4Value);
+  }
+  if(((ch4LastValue -10) <= ch4Value)or (ch4Value < -25)){
+    controlLenker(ch4Value);
+  }else if (ch4Value == 0){
+    controlLenker(ch4Value);
+  }
+
+
+  if (sum != 0 ){
+    mcp1.digitalWrite(ReceiveLED, HIGH);
+  }else {
+    mcp1.digitalWrite(ReceiveLED, LOW);
+  }
+
+  if (mcp1.digitalRead(EngineStart)== HIGH){ 
+    Serial.println("ENGINE START");
+  }
+  // if (mcp1.digitalRead(28) == LOW){
+  //   Serial.println("pressed");
+  // }else{
+  //   Serial.println(" not pressed");
+
+  // }
+
+  // if (ch1Value > 50 ){
+  //   Serial.println("HIGH");
+  //   mcp1.digitalWrite(28, HIGH);
+  // }else{
+  //   mcp1.digitalWrite(28, LOW);
+  // }
+  // time = time + 500; 
+  ch4LastValue = ch4Value; 
+  delay(100);
 }
+#pragma endregion
